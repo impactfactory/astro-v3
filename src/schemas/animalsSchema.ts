13e7764b453efc67795d3fb478984e3978815dff
ImpactFactory <webmaster@impactfactory.ch>
2023-02-18 17:10:02 +0100

    animals

--- src/schemas/animalsSchema.ts
+++ src/schemas/animalsSchema.ts
@@ -5,6 +5,14 @@ import { z } from "astro:content";
 export const animalsSchema: any = z.object({
   name: z.string(),
   draft: z.boolean(),
+  icon: z
+    .object({
+      name: z.string().default(""),
+      cc: z.string().default(""),
+    })
+    .default({}),
+  prey: z.array(z.string()).default([]),
+  vegis: z.array(z.string()).default([]),
   /*desc: z.string().optional(),
   //,
   keyvisual: z
